ca4655b9cb4d7bea9b64d2ea1c398c679ab52daa
Ivan <fokpost@yandex.ru>
2021-01-09 20:46:00 +0300

    lesson done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 "use strict";
+// to string 
+
+//1
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+//2
+console.log(typeof(null + ''));
+
+const num = 3;
+console.log('https://vk.com/catalog/' + num);
+
+const fontSize = 26 + 'px';
+
+//to number
+//1
+console.log(typeof(Number('4')));
+//2
+console.log(typeof(+'5'));
+//3
+console.log(typeof(parseInt('15px', 10)));
+
+let answer = +prompt("Hello", "");
