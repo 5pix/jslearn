12754474297c83adea680e6a52fda48c90e9075a
Ivan <fokpost@yandex.ru>
2021-01-09 20:46:58 +0300

    save last change

--- js/script.js
+++ js/script.js
@@ -22,3 +22,25 @@ console.log(typeof(+'5'));
 console.log(typeof(parseInt('15px', 10)));
 
 let answer = +prompt("Hello", "");
+
+//to boolean 
+
+//0, '', null, NaN, undefined
+//1
+let switcher = null;
+if (switcher) {
+    console.log('Working...');
+    
+}
+
+switcher = 1;
+if (switcher) {
+    console.log('Working...');
+    
+}
+
+//2 
+console.log(typeof(Boolean('4')));
+
+//3
+console.log(typeof(!!"444444"));
