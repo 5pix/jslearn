09408ec9b82c277b4b12b70830aac3eafab8b351
Ivan <fokpost@yandex.ru>
2021-01-12 02:02:51 +0300

    start lesson #29

--- js/script.js
+++ js/script.js
@@ -1,21 +1,9 @@
 "use strict";
 
- const box = document.getElementById("box");
-// console.log(box);
-
-const btns = document.getElementsByTagName('button'); //коллекция элементов
-// console.log(btns);
-
-const circles = document.getElementsByClassName('circle');
-// console.log(circles);
-
-const hearts = document.querySelectorAll('.heart');
-
-// hearts.forEach(item => {
-//     console.log(item);
-// });
-
-const oneHeart = document.querySelector('div');
-
+ const box = document.getElementById("box"),
+    btns = document.getElementsByTagName('button'),//коллекция элементов
+    circles = document.getElementsByClassName('circle'),
+    hearts = document.querySelectorAll('.heart'),
+    oneHeart = document.querySelector('div');
 console.log(oneHeart);
 
