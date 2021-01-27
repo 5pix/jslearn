b13922895288c7658e06673d3f99c695239aff2e
Ivan <fokpost@yandex.ru>
2021-01-28 02:42:05 +0300

    done #35

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 'use strict';
 
+const p = document.querySelectorAll('p');
+console.log(p);
+
+
+
+function loadScript(src) {
+    const script = document.createElement('script');
+script.src = src;
+script.async = false; //убираем сво-во по-умолчю и скрипт грузится как обыно, последовательно
+document.body.append(script);
+}
+loadScript("js/test.js");
+loadScript("js/some.js");
