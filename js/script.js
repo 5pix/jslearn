1ffe76719cf24f756360602cdb170f674f9a324a
Ivan <fokpost@yandex.ru>
2021-05-03 01:49:13 +0300

    end

--- js/script.js
+++ js/script.js
@@ -1,99 +1,32 @@
 "use strict";
 
-window.addEventListener("DOMContentLoaded", () => {
-// tabs
-    const tabs = document.querySelectorAll('.tabheader__item'),
-        tabsContent = document.querySelectorAll('.tabcontent'),
-        tabsParent = document.querySelector('.tabheader__items');
+const box = document.querySelector('.box'),
+      btn = document.querySelector('button');
 
-        function hideTabConent() {
-            tabsContent.forEach(item => {
-                item.classList.add('hide'); 
-                item.classList.remove('show', 'fade'); 
-            });
+// const width = box.clientWidth;
+// const height = box.clientHeight;
 
-            tabs.forEach(item => {
-               item.classList.remove("tabheader__item_active"); 
-            });
-        }
+// const width = box.offsetWidth;
+// const height = box.offsetHeight;
 
-        function showTabConent(i = 0) {
-            tabsContent[i].classList.add('show', 'fade');
-            tabsContent[i].classList.remove('hide');
-            tabs[i].classList.add("tabheader__item_active"); 
-        }
+// const width = box.scrollWidth; 
+// const height = box.scrollHeight; //размерм с полосой прокрутки
 
+// console.log(width, height);
 
-        hideTabConent();
-        showTabConent();
+btn.addEventListener('click', function(){
 
-
-        tabsParent.addEventListener('click', (event) => {
-
-            const target = event.target;
-            
-            if (target && target.classList.contains('tabheader__item')) {
-                tabs.forEach((item, i) => {
-                    if (target == item) {
-                        hideTabConent();
-                        showTabConent(i);
-                    }
-                });
-            }
-        });
-
-        // timer
-
-        const deadline = '2021-03-15';
-        
-        function getTimeRemaining(endtime) {
-            const t = Date.parse(endtime) - Date.parse(new Date()), 
-            
-            days = Math.floor(t / (1000 * 60 * 60 * 24)),
-            hours = Math.floor( (t / (1000 * 60 * 60) % 24)),
-            minutes = Math.floor( (t / 1000 / 60) % 60),
-            seconds = Math.floor( (t / 1000) % 60);
-            
-            // return Object for normal view
-            return {
-                'total': t,
-                'days': days,
-                'hours': hours,
-                'minutes': minutes,
-                'seconds': seconds
-            };      
-        }
-
-        function getZero(num) {
-            if (num >= 0 && num < 10) {
-                return `0${num}`;
-            } else {
-                return num;
-            }
-        }
-
-        function setClock(selector, endtime) {
-            const timer = document.querySelector(selector),
-            days = timer.querySelector('#days'),
-            hours = timer.querySelector('#hours'),
-            minutes = timer.querySelector('#minutes'),
-            seconds = timer.querySelector('#seconds'),
-            timeInterval = setInterval(updateClock, 1000);
-        
-           updateClock(); // start  updateClock if user update page
-            function updateClock(){
-               const t = getTimeRemaining(endtime);
-               
-               days.innerHTML = getZero(t.days);
-               hours.innerHTML = getZero(t.hours);
-               minutes.innerHTML = getZero(t.minutes);
-               seconds.innerHTML = getZero(t.seconds);
-            
-                if (t.total <= 0) {
-                    clearInterval(timeInterval);
-                }
-            }
-        }
-
-       setClock('.timer', deadline); 
+    // box.style.height = box.scrollHeight + 'px';
+    console.log(box.scrollTop);
+    
 });
+
+console.log(box.getBoundingClientRect()); // получение координат элемента
+
+const style = window.getComputedStyle(box);
+console.log(style.display);
+
+
+console.log(document.documentElement.scrollTop); // посмотреть сколько прокручено на странице всего документа
+
+
